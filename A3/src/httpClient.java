d87ceb29ab8acd383b5fe21f39a232daa8554bc5
Mitzn1104 <55814791+Mitzn1104@users.noreply.github.com>
2020-11-28 14:22:35 -0500

    A3

---
+++ A3/src/httpClient.java
@@ -0,0 +1,406 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.ArrayList;
+public class httpClient {
+	
+	BufferedWriter bw = null;
+	BufferedReader br = null;
+	BufferedWriter bwF = null;
+	boolean dflag = false;
+	boolean fflag = false;
+	BufferedReader brFile = null;
+	String data ="";
+	int PORT = 80;
+    String USER_AGENT = "Concordia-HTTP/1.0";
+    String httpVersion = "HTTP/1.0";
+	
+
+	public String getRequest(String url, ArrayList<String> headers,boolean displayHeader, int redirectionAttempt) {
+	
+		String responseString ="";
+		String redirectResponseString ="";
+		String host ="";
+		String path ="";
+		CharSequence status301 = "301";
+		try {
+			//Set host and path
+			if (url.isEmpty()) {
+	            System.out.println("URL is missing");
+	            return "ERROR";
+	        } else {
+	            int separator = url.indexOf("/", 7);
+	            if (separator == -1) {
+	                host = url.substring(7, url.length());
+	            } else {
+	                host = url.substring(7, separator);
+	                if(host.contains("localhost"))
+	                	host ="localhost";
+	                path = url.substring(separator);
+	            }
+	        }
+			
+			//System.out.println("host"+host);
+			//System.out.println("path"+path);
+			if(path.equals(""))
+				path="/";
+			
+			//Connection TCP
+			Socket socket = new Socket(InetAddress.getByName(host), PORT);
+			
+			//Build Request
+			bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF8"));
+			//System.out.println("GET" + " " +path + " " + httpVersion +"\r\n");
+            bw.write("GET" + " " +path + " " + httpVersion +"\r\n");
+            bw.write("Host: " + host+"\r\n");
+            bw.write("User-Agent: " + USER_AGENT +"\r\n");
+            if (fflag)
+                bw.write("Content-Type: multipart/form-data; boundary=" + "***" + "\r\n");
+            //Attach headers
+            if (!headers.isEmpty()) {
+                for (String header : headers) {
+                    bw.write(header + "\r\n");
+                }
+            }
+            
+            bw.write("\r\n");
+            bw.flush();
+            
+            //Response
+            br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            String responseLine;
+            String responseHeader = "";
+            String responseBody = "";
+            Boolean redirectTo = false;
+            Boolean redirectStatus = false;
+            boolean headerCheck = false;
+            while ((responseLine = br.readLine()) != null) {
+            	if (!redirectStatus && responseLine.contains(status301)) {
+            		redirectStatus = true;
+                    redirectTo = true;
+                }
+            	if (!headerCheck && responseLine.equals("")) {
+            		headerCheck = true;
+            	}
+            	if (!headerCheck) {
+                    responseHeader += responseLine + "\n";
+                } else {
+                    responseBody += responseLine + "\n";
+                }
+            	
+            	//Redirect Code
+                if (redirectTo && responseLine.length() > 10 && responseLine.substring(0, 10).equals("Location: ")) {
+                    String redirectLocation = responseLine.substring(10, responseLine.length());
+                    url = redirectLocation;                     
+                    System.out.println("Redirecting to:" + url );
+                    if(redirectionAttempt <=5)
+                    redirectResponseString = getRequest(url, headers,displayHeader,++redirectionAttempt);
+                    break;
+                }
+            }
+			
+                        
+            if(displayHeader)
+            	responseString += responseHeader;
+            responseString += responseBody;
+            
+			}catch(Exception e) {
+				e.printStackTrace();
+				System.out.println("ERROR in command");
+		}
+		
+		if(!redirectResponseString.equals(""))
+			return redirectResponseString;
+		else
+			return responseString;
+	}
+
+	public String postRequest(String url, ArrayList<String> headers, Boolean displayHeader ,String data) {
+	
+		String responseString ="";
+		String host ="";
+		String path ="";
+		try {
+			if (url.isEmpty()) {
+	            System.out.println("URL is missing");
+	            return "ERROR";
+	        } else {
+	            int separator = url.indexOf("/", 7);
+	            if (separator == -1) {
+	                host = url.substring(7, url.length());
+	            } else {
+	                host = url.substring(7, separator);
+	                if(host.contains("localhost"))
+	                	host ="localhost";
+	                path = url.substring(separator);
+	            }
+	        }
+			
+			//System.out.println("host"+host);
+			//System.out.println("path"+path);
+	
+		//Connection TCP
+		Socket socket = new Socket(InetAddress.getByName(host), PORT);
+		
+		//Build Request
+		bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF8"));
+        bw.write("POST" + " " + path + " " + httpVersion +"\r\n");
+        bw.write("Host: " + host+"\r\n");
+        bw.write("User-Agent: " + USER_AGENT +"\r\n");
+        if (fflag)
+            bw.write("Content-Type: multipart/form-data; boundary=" + "***" + "\r\n");
+        //Attach headers
+        if (!headers.isEmpty()) {
+            for (String header : headers) {
+                bw.write(header + "\r\n");
+            }
+        }
+        //Add data
+        if (!data.isEmpty()) {
+        	if(data.contains(":") && data.startsWith("{") && data.endsWith("}")) 
+        		data = convertToJson(data);
+        	bw.write("Content-Length: " + data.length()+ "\r\n" );
+                	
+        }
+        if(!host.equals("localhost")) {
+        	bw.write("\r\n");
+        	bw.write(data+"\r\n");
+        }
+        else {
+        	bw.write("\n");
+        	bw.write("Data:"+data+"\r\n");
+        }
+        //System.out.println(data);
+        
+        bw.flush();
+        
+        //Response
+        br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+        String responseLine;
+        String responseHeader = "";
+        String responseBody = "";
+        boolean headerCheck = false;
+        while ((responseLine = br.readLine()) != null) {
+        	if (!headerCheck && responseLine.equals("")) {
+        		headerCheck = true;
+        	}
+        	if (!headerCheck) {
+                responseHeader += responseLine + "\n";
+            } else {
+                responseBody += responseLine + "\n";
+            }
+        }
+		
+        
+        //Display Result
+        if(displayHeader)
+        	responseString += responseHeader;
+        responseString += responseBody;
+        
+		}catch(Exception e) {
+			e.printStackTrace();
+			System.out.println("ERROR in command");
+	}
+
+		return responseString;
+	}
+
+
+	public String processRequest(String args[]) {
+		
+		String response ="";
+		String url ="";
+		ArrayList<String> headers = new ArrayList<String>();
+		String method = args[0].equals("get") ? "GET" : "POST";
+		int argLength = args.length;
+		boolean writeInFile = false;
+		boolean displayHeader = false;
+		
+		try {
+			for (int i = 1; i < argLength; ++i) {
+	            String token = args[i];
+	            switch (token) {
+	            case "-o":
+                    if (argLength > i++ && !args[i].startsWith("http://" )) {
+                        String oFilename = args[i];
+                        File oFile = new File(oFilename);
+                        oFile.createNewFile();
+                        bwF = new BufferedWriter(new FileWriter(oFile));
+                        writeInFile = true;
+                        
+                    } else {
+                        System.out.println("ERROR : -o: missing a output file name");
+                        return "ERROR";
+                    }
+                    break;
+	            case "-d":
+                    if (method.equals("GET")) {
+                        System.out.println("ERROR : -d: GET does not allow inline data");
+                        return "ERROR";
+                    } else {
+                        if (dflag) {
+                            System.out.println("ERROR :-d: duplicate option");
+                            return "ERROR";
+                        } else if (fflag) {
+                            System.out.println("ERROR: -d: -d and -f are not allowed together");
+                            return "ERROR";
+                        } else if (argLength > i++ && !args[i].startsWith("http://" ) ) {
+                            data = args[i] ;
+                            dflag = true;
+                        } else {
+                            System.out.println("ERROR :-d: missing inline data");
+                            return "ERROR";
+                        }
+                    }
+                    break;
+	            case "-f":
+                    if (method.equals("GET")) {
+                        System.out.println("ERROR: -f: GET does not allow file data");
+                        return "ERROR";
+                    } else {
+                        if (fflag) {
+                            System.out.println("ERROR : -f: duplicate option");
+                            return "ERROR";
+                        } else if (dflag) {
+                            System.out.println("ERROR : -f: -d and -f are not allowed together");
+                            return "ERROR";
+                        } else if (argLength > i++ && !args[i].startsWith("http://" ) ) {
+                            String filename = args[i];
+                            File file = new File(filename);
+                            String content = "";
+                            if (file.exists()) {
+                            	brFile = new BufferedReader(new FileReader(file));
+                                String line;
+                                while ((line = brFile.readLine()) != null) {
+                                    content += line + "\r\n";
+                                }
+                                data += "--" + "***" + "\r\n";
+                                data += "Content-Disposition: form-data; name=\"file\"; filename=" + filename + "\r\n";
+                                data += "Content-Type: text/plain" + "\r\n";
+                                data += "Content-Length:" + content.length() + "\r\n";
+                                data += "\r\n";                                                             
+                                data += content + "\r\n";
+                                data += "--"  +"***" + "--" +"\r\n";
+                                fflag = true;
+                            } else {
+                                System.out.println(filename + "File does not exist");
+                                return "ERROR";
+                            }
+                        } else {
+                            System.out.println("ERROR : -f: missing file name");
+                            return "ERROR";
+                        }
+                    }
+                    break;
+	            case "-h":
+                    if (argLength > i++ && args[i].contains(":") && !args[i].startsWith("http://" ) ) {
+                        headers.add(args[i]);
+                    } else {
+                        System.out.println("ERROR : -h: missing or incorrect key:value pair(s)");
+                        return "ERROR";
+                    }
+                    break;
+	            case "-v":
+	            	displayHeader = true;
+	            	break;
+	             default :
+	            	 if (url.isEmpty()) {
+	                     if (token.length() > 6 && token.startsWith("http://" )) {
+	                         url = token;
+	                         if(token.contains("localhost")) {
+	                        	 String tempPort = token.substring(token.lastIndexOf(":"),token.length());
+	                        	 PORT = Integer.parseInt(tempPort.substring(1,tempPort.indexOf("/")));
+	                        	 //System.out.println("port number :"+PORT);
+	                         }
+	                     } else {
+	                         System.out.println(token + ": invalid command");
+	                         return "ERROR";
+	                     }
+	                 } else {
+	                     System.out.println(token + ": invalid command");
+	                     return "ERROR";
+	                 }
+	                 break;
+	            }
+			}
+		
+		if(method == "GET") {
+			
+			response = getRequest(url, headers,displayHeader,1);
+		}
+		else
+			response = postRequest(url,headers,displayHeader,data);
+		
+		if(writeInFile) {
+			try {
+				bwF.write(response);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			response ="";
+		}
+		
+		}catch(Exception e) {
+			e.printStackTrace();
+			System.out.println("ERROR in command");
+		}
+	 finally {
+        try {
+            if (null != bw) {
+                bw.close();
+            }
+            if (null != br) {
+                br.close();
+            }
+            if (null != brFile) {
+            	brFile.close();
+            }
+            if (null != bwF) {
+                bwF.close();
+            }
+        } catch (IOException ioe) {
+        	System.out.println("ERROR in command");
+        }
+    }
+		return response;
+	}
+	
+
+	private String convertToJson(String data) throws Exception {
+		// TODO Auto-generated method stub
+		String formattedJSON = "{";
+		data = data.replace("{", "");
+		data = data.replace("}", "");
+		String[] dataArray = data.split(",");
+		
+		for(String d : dataArray) {
+			String[] dataKeyValue = d.split(":");
+			
+			Boolean numeric = true;
+			try {
+	           Double.parseDouble(dataKeyValue[1]);
+	        } catch (NumberFormatException e) {
+	            numeric = false;
+	        }
+			
+			if(numeric)
+				formattedJSON += "\""+ dataKeyValue[0].trim() + "\":" +  dataKeyValue[1].trim() + ",";
+			else
+				formattedJSON += "\""+ dataKeyValue[0].trim() + "\":" + "\""+ dataKeyValue[1].trim() + "\",";
+
+		}
+		formattedJSON = formattedJSON.substring(0, formattedJSON.length() - 1) +"}";
+		return formattedJSON;
+	}
+	
+}
---
+++ A3/src/httpc.java
@@ -0,0 +1,69 @@
+
+public class httpc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String result= "";
+		if (args.length == 0 || (args.length == 1 && args[0].equals("help"))) {
+            String outputString = "\n";
+            outputString += "httpc is a curl-like application but supports HTTP protocol only.\n";
+            outputString += "Usage: \n    httpc command [arguments]\nThe commands are:\n";
+            outputString += "    get     executes a HTTP GET request and prints the response.\n";
+            outputString += "    post    executes a HTTP POST request and prints the response.\n";
+            outputString += "    help    prints this screen.\n\n";
+            outputString += "Use \"httpc help [command]\" for more information about a command.\n";
+            System.out.println(outputString);
+        } else if (args.length == 1) {
+            if (args[0].equals("get") || args[0].equals("post")) {
+                System.out.println(args[0] + ": URL required");
+            } else {
+                System.out.println(args[0] + ": invalid command");
+            }
+        } else {
+            switch (args[0]) {
+                case "help":
+                    switch (args[1]) {
+                        case "get":
+                            String getString = "\n";
+                            getString += "usage: httpc get [-v] [-h key:value] URL\n\n";
+                            getString += "Get executes a HTTP GET request for a given URL.\n\n";
+                            getString += "    -v           Prints the detail of the response such as protocol, status, and headers.\n";
+                            getString += "    -h key:value Associates headers to HTTP Request with the format 'key:value'.\n";
+                            System.out.println(getString);
+                            break;
+                        case "post":
+                            String postString = "\n";
+                            postString += "usage: httpc post [-v] [-h key:value] [-d inline-data] [-f file] URL\n\n";
+                            postString += "Post executes a HTTP POST request for a given URL with inline data or from file.\n\n";
+                            postString += "    -v             Prints the detail of the response such as protocol, status, and headers.\n";
+                            postString += "    -h key:value   Associates headers to HTTP Request with the format 'key:value'.\n";
+                            postString += "    -d string          Associates an inline data to the body HTTP POST request.\n";
+                            postString += "    -f file            Associates the content of a file to the body HTTP POST request.\n\n";
+                            postString += "Either [-d] or [-f] can be used but not both.\n";
+                            System.out.println(postString);
+                            break;
+                        default:
+                            System.out.println(args[0] + ": invalid command");
+                            break;
+                    }
+                    break;
+                case "get": 
+                	httpClient httpGet = new httpClient();
+                	result = httpGet.processRequest(args);
+                	break;
+                
+                case "post": 
+                	httpClient httpPost = new httpClient();
+                	result = httpPost.processRequest(args);
+                    break;
+                
+                default:
+                    System.out.println(args[0] + ": invalid command");
+                    break;
+            }
+        }
+
+		System.out.println(result);
+
+	}
+}
